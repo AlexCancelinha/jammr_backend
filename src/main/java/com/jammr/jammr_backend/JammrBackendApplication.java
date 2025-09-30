f60cc57074f929ac7a80cbbfda11cb1eb05fe480
Alexandre_Cancelinha <alexctra@hotmail.com>
2025-09-30 18:48:10 +0100

    initial commit

---
+++ src/main/java/com/jammr/jammr_backend/JammrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jammr.jammr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JammrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JammrBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jammr/jammr_backend/JammrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jammr.jammr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JammrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
